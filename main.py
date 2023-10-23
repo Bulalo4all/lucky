b6283a859a8f843c7fe784587b8cdde256a9f6c0
Bulalo4all <147882966+Bulalo4all@users.noreply.github.com>
2023-10-23 17:40:51 -0600

    Update main.py
    Writing the first part of code, print line at the end verifies the code is counting the letters accurately

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#importing string library to help with counting letters
+import string
 
+# context manager
+with open("mydefaults.ini.txt") as ini_file:
+    data = ini_file.read()
+
+#Create the beginning of a number count
+number_of_letters = 0
+
+#create a variable to store ascii letters
+alphabet = string.ascii_letters
+
+#create a list with each line of data from ini file
+lines = data.split("\n")
+
+#for loop to count the number of letters in the ini file
+for line in lines:
+    for letter in line:
+        if letter in alphabet:
+            number_of_letters += 1
+    print(line, number_of_letters)
