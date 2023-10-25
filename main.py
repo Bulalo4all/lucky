ca6467bb2b011a697edc63f11332487c6280c722
Bulalo4all <147882966+Bulalo4all@users.noreply.github.com>
2023-10-24 21:05:30 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ number_of_letters = 0
 number_of_numbers = 0
 
 #create a variable to store ascii letters and digits
-alphabet = string.ascii_letters
+# alphabet = string.ascii_letters
 number_0_to_nine = string.digits
 
 #create a list with each line of data from ini file
@@ -19,9 +19,15 @@ lines = data.split("\n")
 #for loop to count the number of letters and digits in the ini file
 for line in lines:
     for letter in line:
-        if letter in alphabet:
+        if letter in string.ascii_letters:
             number_of_letters += 1
         elif letter in number_0_to_nine:
             number_of_numbers += 1
             
 print(number_of_letters, number_of_numbers)
+
+# try writing out to file with the open function
+# supply a file mode to open
+# file_output = open(name of file, file mode)
+# use the method write on the file handle returned from the open function
+
